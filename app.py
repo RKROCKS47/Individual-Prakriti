bdcaed6cbcb56f4b2034d76e708f1b0190b8505a
RKROCKS47 <78202463+RKROCKS47@users.noreply.github.com>
2025-12-28 16:03:47 +0530

    Fixed clean requirements.txt
    Fixed clean requirements.txt for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request,jsonify
 import json
 import joblib
@@ -442,4 +443,4 @@ def get_live_location():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=False)
